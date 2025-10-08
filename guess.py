13bf0573a34e7dace62230d7cca054f5183dca98
rasuloff789 <rasuloff789@gmail.com>
2025-10-08 15:13:36 +0500

    Add new feature X

--- guess.py
+++ guess.py
@@ -1,9 +1,11 @@
-import random
-
-n = random.randint(0, 10)
-guess = int(input("guess a number between 0 and 10: "))
-
-while True:
-    if n == guess:
-        print("Topdingingiz :")
-        break
+import requests
+r = requests.get('https://iamawesome.com')
+print(r.text)
+# r.headers['content-type']
+# 'application/json; charset=utf8'
+# r.encoding
+# 'utf-8'
+# r.text
+# '{"authenticated": true, ...'
+# r.json()
+# {'authenticated': True, ...}
